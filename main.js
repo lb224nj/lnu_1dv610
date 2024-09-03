7f9a7533d9f3183367f1f2993ce63a11410a6393
Love Barrehag <lb224nj@student.lnu.se>
2024-09-03 14:54:37 +0200

    Begin logic for displaying interesting fact to user

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log('It is working!')
+document.getElementById('surpriseButton').addEventListener('click', function(surpriseEvent) {
+const user = document.getElementById('enterName').value
+if (user.length === 1) {
+    
+}
+})
