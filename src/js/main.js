2fff229ac6bf87c2d057a13b33e0ee2acd479bb9
Love Barrehag <lb224nj@student.lnu.se>
2024-09-03 16:37:48 +0200

    Use src folder for better structure

--- src/js/main.js
+++ src/js/main.js
@@ -2,16 +2,21 @@ console.log('It is working!')
 document.getElementById('surpriseButton').addEventListener('click', function(surpriseEvent) {
   surpriseEvent.preventDefault()
 const user = document.getElementById('enterName').value
+const angryEmoji=document.getElementById('angryEmoji')
 if (user === '') {
-    document.getElementById('displayFact').textContent = 'You did not entar a name. Therefore you dont get an intersting fact. Instead you get this:'
-    const angryEmoji=document.getElementById('angryEmoji')
+    document.getElementById('displayFact').textContent = 'You did not enter a name. Therefore you do not get an intersting fact. Instead you get this:'
+    
     angryEmoji.classList.remove('hidden')
-} else if (user.length === 1) {
+} else {
+  document.getElementById('angryEmoji').classList.add('hidden')
+
+ if (user.length === 1) {
     document.getElementById('displayFact').textContent = 'This is interesting fact number one: JavaScript was invented in 1995. Can you figure out why you got this specific fact?'
 } else if (user.length === 2) {
         document.getElementById('displayFact').textContent = 'This is the second interesting fact'
     } else if (user.length === 3) {
         document.getElementById('displayFact').textContent = 'This is the third interesting fact'
-    } 
+    }
+  } 
 
 })
