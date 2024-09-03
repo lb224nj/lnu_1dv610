abb74cd9f9e9dd2fb7feac857b0feb4230cc47cb
Love Barrehag <lb224nj@student.lnu.se>
2024-09-03 15:18:03 +0200

    Add main.js script to index.html and use textcontent to display first interesting fact

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 console.log('It is working!')
 document.getElementById('surpriseButton').addEventListener('click', function(surpriseEvent) {
+  surpriseEvent.preventDefault()
 const user = document.getElementById('enterName').value
 if (user.length === 1) {
-    
+    document.getElementById('displayFact').textContent = 'This is the first interesting fact'
 }
 })
